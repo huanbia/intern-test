fedc151f707e601c53a7416bbe91d8115aae076b
huanbia <huanbia@163.com>
2018-02-26 15:34:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -- coding: utf-8 --
+import sys
 def divide(a,b):
-  if b==0:
-    raise Exception("除数不能为0")
+  if b == 0:
+    print("除数不能为0")
+    sys.exit(1)
   return a*1.0/b
 
 if __name__ == "__main__":
   a = int(input(u"输入被除数: "))
   b = int(input(u"输入除数: "))
   print(divide(a,b))
-  
