bc45b8103b118ff2ffc691637c708eba1f1e69ea
huanbia <huanbia@163.com>
2018-02-26 12:24:47 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# -- coding: utf-8 --
+def divide(a,b):
+  if b==0:
+    raise Exception("除数不能为0")
+    retun
+  return a*1.0/b
+
+if __name__ == "__main__":
+  a = int(input(u"输入被除数: "))
+  b = int(input(u"输入除数: "))
+  print(divide(a,b))
+  
